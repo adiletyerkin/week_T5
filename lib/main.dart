f212f00ab4e714079892536508925430df5430ec
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-10-14 23:41:52 +0600

    Task_5

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue[50],
+        appBar: AppBar(
+          title: Text('I am poor app'),
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/poor_man_app.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
